567c33e251cbb9b5506664fd2efddb131916c496
duanzhengqiang <duanzhengqiang@apache.org>
2026-03-01 11:21:58 +0800

    refactor: Use IntelliSql to replace IntelliSQL in license and update spotless copyright.txt

--- intellisql-jdbc/src/main/java/com/intellisql/jdbc/IntelliSqlDriver.java
+++ intellisql-jdbc/src/main/java/com/intellisql/jdbc/IntelliSqlDriver.java
@@ -1,8 +1,8 @@
 /*
- * Licensed to the IntelliSQL Project under one or more
+ * Licensed to the IntelliSql Project under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
- * The IntelliSQL Project licenses this file to You under the Apache License, Version 2.0
+ * The IntelliSql Project licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
  *
